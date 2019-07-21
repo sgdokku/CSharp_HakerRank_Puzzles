1a1a41ed544d9bba4d090eb379335b81b2bdfa73
Sai Gopinath Dokku <saigopinathdokku@gmail.com>
2019-07-21 08:58:17 +0530

    first commit

---
+++ JumpingOnClouds/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * 
+    Function Description
+
+    Complete the jumpingOnClouds function in the editor below. 
+    It should return the minimum number of jumps required, as an integer.
+
+    jumpingOnClouds has the following parameter(s):
+        c: an array of binary integers
+
+    Input Format
+        The first line contains an integer n, the total number of clouds. 
+        The second line contains n space-separated binary integers describing clouds c[i] where 0 <= i < n.
+
+    Constraints
+        2 <= n <= 100
+        c[i] = 0 or 1
+        c[0] = c[n-1] = 0
+
+    Output Format
+        Print the minimum number of jumps needed to win the game.
+
+        Sample Input 0
+            7
+            0 0 1 0 0 1 0
+        Sample Output 0
+            4
+        Explanation 0: 
+            Emma must avoid c[2] and c[5]. She can win the game with a minimum of 4 jumps:
+
+
+        Sample Input 1
+            6
+            0 0 0 0 1 0
+        Sample Output 1
+            3
+ */
+
+namespace JumpingOnClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter("output.txt", false);
+            int n = Convert.ToInt32(Console.ReadLine());
+            textWriter.WriteLine("no. of clouds : " + n);
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            textWriter.WriteLine("cloud sequence : " + string.Join(",", ar));
+            textWriter.WriteLine("no. of jumps : " + jumpingOnClouds(ar));
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        // Complete the jumpingOnClouds function below.
+        static int jumpingOnClouds(int[] c)
+        {
+            if (c[0] > 0 || c[c.Length - 1] > 0)
+                return -1;
+            int hop = 0;
+            for (int x = 0; x < c.Length;)
+            {
+                for(int y = x + 1; ;)
+                {
+                    if(y >= c.Length)
+                    {
+                        x += 1;
+                        break;
+                    }
+                    if((y + 1 < c.Length) && c[y+1] == 0)
+                    {
+                        hop++;
+                        x += 2;
+                        break;
+                    } else if (c[y] == 0)
+                    {
+                        hop++;
+                        x += 1;
+                        break;
+                    } else
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return hop;
+        }
+    }
+}
---
+++ SockMerchant/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+/*
+ * Function Description
+
+    Complete the sockMerchant function in the editor below. 
+    It must return an integer representing the number of matching pairs of socks that are available.
+
+    sockMerchant has the following parameter(s):
+
+        n: the number of socks in the pile
+        ar: the colors of each sock
+
+    Input Format
+
+        The first line contains an integer , the number of socks represented in . 
+        The second line contains  space-separated integers describing the colors  of the socks in the pile.
+
+    Constraints
+        1 <= n <= 100
+        1 <= ar[i] <= 100 where 0 <= i < n
+
+    Output Format
+        Return the total number of matching pairs of socks that John can sell.
+
+    Sample Input
+        9
+        10 20 20 10 10 30 50 10 20
+
+    Sample Output
+        3
+ */
+namespace SockMerchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter("output.txt", false);
+            int n = Convert.ToInt32(Console.ReadLine());
+            textWriter.WriteLine("no. of socks : " + n);
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            textWriter.WriteLine("socks are : " + string.Join(",", ar));
+            int result = sockMerchant(n, ar);
+            textWriter.WriteLine("no. of pairs : " + result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        // Complete the sockMerchant function below.
+        static int sockMerchant(int n, int[] ar)
+        {
+            int nPairs = 0;
+            ConcurrentDictionary<int, int> keyValuePairs = new ConcurrentDictionary<int, int>();
+            if (n >= 1 && n <= 100)
+            {
+                for (int i = 0; i >= 0 && n > i; i++)
+                {
+                    keyValuePairs.AddOrUpdate(ar[i], 1, (k, v) => v + 1);
+                }
+            }
+            foreach (int i in keyValuePairs.Values)
+            {
+                if(i > 1)
+                {
+                    nPairs += i / 2;
+                }
+            }
+            return nPairs;
+        }
+    }
+}
